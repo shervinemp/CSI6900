e8f4a41b91c3dce3f12a3267a3185e22a133ce7f
shervinemp <shervin.naseri@gmail.com>
2022-08-11 17:53:24 -0400

    add create script

---
+++ implementation/runner/pylot_create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PYLOT_ADDR="${PWD%/*}/pylot"
+nvidia-docker run -itd --name pylot -p 20022:22 -v "$PYLOT_ADDR:/home/erdos/workspace/pylot/pylot" erdosproject/pylot /bin/bash
+nvidia-docker cp ~/.ssh/id_rsa.pub pylot:/home/erdos/.ssh/authorized_keys
+nvidia-docker exec -i -t pylot sudo chown erdos /home/erdos/.ssh/authorized_keys
+nvidia-docker exec -i -t pylot sudo service ssh start
