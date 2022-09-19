1137da4af165839f123d1ffded5b0a0896f30df6
shervinemp <shervin.naseri@gmail.com>
2022-09-19 16:04:00 -0400

    Store flakiness for each repition

--- implementation/runner/mscripts/event_flaky_plot.py
+++ implementation/runner/mscripts/event_flaky_plot.py
@@ -38,13 +38,13 @@ if __name__ == '__main__':
             event6(input_df),
         ]
         cum_dfs.append(arr)
-    sel = pd.DataFrame(ind_dfs, columns=['idx'] + ind_cols).groupby('idx')
-    ind_mean_df = sel.mean().add_suffix('_mean')
-    ind_min_df = sel.min().add_suffix('_min')
-    ind_max_df = sel.max().add_suffix('_max')
-    ind_df = pd.concat([ind_mean_df, ind_min_df, ind_max_df], axis=1)
+    ind_df = pd.DataFrame(ind_dfs, columns=['idx'] + ind_cols).set_index('idx')
+    ind_mean_df = ind_df.groupby('idx').mean().add_suffix('_mean')
+    ind_min_df = ind_df.groupby('idx').min().add_suffix('_min')
+    ind_max_df = ind_df.groupby('idx').max().add_suffix('_max')
+    ind_res_df = pd.concat([ind_mean_df, ind_min_df, ind_max_df], axis=1)
     cum_df = pd.DataFrame(cum_dfs, columns=['idx'] + cum_cols).set_index('idx')
-    event_df = pd.merge(ind_df, cum_df, left_index=True, right_index=True)
+    event_df = pd.merge(ind_res_df, cum_df, left_index=True, right_index=True)
     sf_df = soft_flaky(whole_df).add_suffix('_soft')
     sf_rang_df = soft_flaky(whole_df, type='range').add_suffix('_rang')
     hf_df = hard_flaky(whole_df).add_suffix('_hard')
@@ -52,6 +52,8 @@ if __name__ == '__main__':
     flaky_df = flaky_df.reindex(new_index).reset_index(drop=True)
     measure_df = pd.merge(event_df, flaky_df, left_index=True, right_index=True)
     measure_df.to_csv('measure.csv')
+    measure_rep_df = pd.merge(ind_df, flaky_df, left_index=True, right_index=True)
+    measure_df.to_csv('measure_rep.csv')
 
     rows, cols = len(flaky_df.columns), len(event_df.columns)
     fig, axes = plt.subplots(rows, cols, figsize=(4*cols, 4*rows))
